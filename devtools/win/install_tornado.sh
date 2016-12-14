c26f4122745fd9cba2722559fe906b61cd536a4c
Hai Nguyen <hainm.comp@gmail.com>
2016-12-14 16:44:29 -0500

    more

--- devtools/win/install_tornado.sh
+++ devtools/win/install_tornado.sh
@@ -3,3 +3,9 @@ cd tornado
 python setup.py build -c mingw32
 python setup.py install
 cd ..
+
+git clone https://github.com/pallets/markupsafe
+cd markupsafe
+python setup.py build -c mingw32
+python setup.py install
+cd ..
