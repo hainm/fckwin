922860ea91b2ff71d536b762a53257626082b917
Hai Nguyen <hainm.comp@gmail.com>
2016-12-14 15:02:28 -0500

    more

--- test.py
+++ test.py
@@ -6,7 +6,10 @@ if sys.platform.startswith('win'):
     os.environ['PATH'] = os.path.dirname(os.path.abspath(__file__)) + ';' + os.environ['PATH']
     msys_bin = '/c/msys64/mingw64/bin/'
     msys_lib = '/c/msys64/mingw64/lib/'
-    os.environ['PATH'] = ';'.join((os.environ['PATH'], msys_bin, msys_lib))
+    msys_usr_bin = '/c/msys64/usr/bin/'
+    msys_usr_lib = '/c/msys64/usr/lib/'
+    os.environ['PATH'] = ';'.join((os.environ['PATH'], msys_bin, msys_lib, msys_usr_bin,
+        msys_usr_lib))
     print('PATH', os.environ['PATH'])
 
 import pytraj
