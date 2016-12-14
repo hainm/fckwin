aef3b1151b9bc83090a9f89f5a56188869cb2d11
Hai Nguyen <hainm.comp@gmail.com>
2016-12-14 15:58:22 -0500

    more

--- test.py
+++ test.py
@@ -4,12 +4,9 @@ if sys.platform.startswith('win'):
     # set PATH to find libcpptraj.lib
     # we copy it (libcpptraj.lib) to pytraj folder during installation.
     os.environ['PATH'] = os.path.dirname(os.path.abspath(__file__)) + ';' + os.environ['PATH']
-    msys_bin = '/c/msys64/mingw64/bin/'
-    msys_lib = '/c/msys64/mingw64/lib/'
-    msys_usr_bin = '/c/msys64/usr/bin/'
-    msys_usr_lib = '/c/msys64/usr/lib/'
-    os.environ['PATH'] = ';'.join((msys_bin, os.environ['PATH'], msys_bin, msys_lib, msys_usr_bin,
-        msys_usr_lib))
+    msys_bin = 'C://msys64/mingw64/bin/'.replace('/', '\\')
+    msys_lib = 'C://msys64/mingw64/lib/'.replace('/', '\\')
+    os.environ['PATH'] = ';'.join((msys_bin, os.environ['PATH'], msys_bin, msys_lib))
     print('PATH', os.environ['PATH'])
 
 import pytraj
