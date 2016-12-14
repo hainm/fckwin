f2275e60c20d56ed2bf8ade0f1912bacdc7c3bc9
Hai Nguyen <hainm.comp@gmail.com>
2016-12-14 16:02:19 -0500

    more

--- test.py
+++ test.py
@@ -6,6 +6,7 @@ if sys.platform.startswith('win'):
     os.environ['PATH'] = os.path.dirname(os.path.abspath(__file__)) + ';' + os.environ['PATH']
     msys_bin = 'C:/msys64/mingw64/bin/'.replace("/", "\\")
     msys_lib = 'C:/msys64/mingw64/lib/'.replace('/', '\\')
+    print(msys_bin, msys_lib)
     os.environ['PATH'] = ';'.join((os.environ['PATH'], msys_bin, msys_lib))
     print('PATH', os.environ['PATH'])
 
