f282efac69d912613bdeae34d71247495ba2adda
Hai Nguyen <hainm.comp@gmail.com>
2016-12-14 17:27:02 -0500

    build_ext

--- devtools/win/download_pip.sh
+++ devtools/win/download_pip.sh
@@ -1,5 +1,3 @@
 wget https://bootstrap.pypa.io/get-pip.py
 python get-pip.py
 python -c "import pip; print(pip)"
-
-python -m pip uninstall setuptools
--- devtools/win/install_notebook_deps.sh
+++ devtools/win/install_notebook_deps.sh
@@ -1,11 +1,11 @@
 git clone https://github.com/tornadoweb/tornado
 cd tornado
-python setup.py build -c mingw32
+python setup.py build_ext -c mingw32
 python setup.py install
 cd ..
 
 git clone https://github.com/pallets/markupsafe
 cd markupsafe
-python setup.py build -c mingw32
+python setup.py build_ext -c mingw32
 python setup.py install
 cd ..
