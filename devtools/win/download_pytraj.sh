aa87e31c45db255c2975015094a3ce10ea3c740e
Hai Nguyen <hainm.comp@gmail.com>
2016-12-14 12:17:46 -0500

    where are you?

--- devtools/win/download_pytraj.sh
+++ devtools/win/download_pytraj.sh
@@ -1,2 +1,4 @@
-appveyor DownloadFile https://ci.appveyor.com/api/buildjobs/04uqpbsvigvc1ha7/artifacts/pytraj.zip -FileName pytraj.zip
-7z x pytraj.zip
+appveyor DownloadFile https://ci.appveyor.com/api/buildjobs/04uqpbsvigvc1ha7/artifacts/pytraj.zip -FileName build.zip
+7z x build.zip
+# I know, I am suck at naming
+mv build/lib*/pytraj .
